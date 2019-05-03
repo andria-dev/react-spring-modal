a0625912e53c60e782327666b0cfc99ab108cb79
Christopher Brown <chbphone55@gmail.com>
2019-05-03 17:59:56 -0600

    setting up microbundle

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: null,
+  rangeEnd: null,
+  filePath: ''
+};
