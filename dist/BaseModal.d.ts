8eaff6b0e591d40fa28192cfaa9ad10c1edcd9fd
Christopher Brown <chbphone55@gmail.com>
2020-02-07 15:39:09 -0600

    build

--- dist/BaseModal.d.ts
+++ dist/BaseModal.d.ts
@@ -3,6 +3,7 @@ export interface IBaseModalProps {
     children: ReactNode;
     isOpen: boolean;
     onRequestClose: () => void;
+    autoFocus?: boolean;
 }
-declare function BaseModal({ isOpen, onRequestClose, children }: IBaseModalProps): JSX.Element;
+declare function BaseModal({ isOpen, onRequestClose, children, autoFocus }: IBaseModalProps): JSX.Element;
 export default BaseModal;
--- dist/ModalBackdrop/index.d.ts
+++ dist/ModalBackdrop/index.d.ts
@@ -1,4 +1,4 @@
 import React from 'react';
 import './style.css';
-declare const _default: React.ForwardRefExoticComponent<Pick<any, string | number | symbol> & React.RefAttributes<{}>>;
+declare const _default: React.ForwardRefExoticComponent<Pick<any, string | number | symbol> & React.RefAttributes<unknown>>;
 export default _default;
