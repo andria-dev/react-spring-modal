b05be58764a8eb48d73cf3fd2a2f09417e06b95e
ChrisBrownie55 <chbphone55@gmail.com>
2020-06-06 10:37:02 -0500

    Totally new setup

---
+++ react-spring-modal/src/index.ts
@@ -0,0 +1,6 @@
+export { ModalPortal } from './shared/ModalPortal';
+export { ModalBackdrop } from './shared/ModalBackdrop';
+
+export { BaseModal } from './shared/BaseModal';
+export { CenterModal, useCenterModalTransition } from './index/CenterModal';
+export { BottomModal, useBottomModalTransition } from './index/BottomModal';
